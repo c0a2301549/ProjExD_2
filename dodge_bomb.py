26d84225151cc88d602974db92cb24ad8872dccf
C0A23015 <c0a2301549@edu.teu.ac.jp>
2024-10-01 15:16:25 +0900

    GameOver

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -51,6 +51,8 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        if kk_rct.colliderect(bb_rct):#こうかとんと爆弾が重なった時
+            return #ゲームオーバー
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0] #横、縦
